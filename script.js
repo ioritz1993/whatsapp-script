08e77d19fc4ead40c99097ba91ca7da3e1f8e447
Ioritz <46922333+ioritz1993@users.noreply.github.com>
2023-12-03 15:30:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 async function sendScript(scriptText) {
-    const lines = scriptText.split(/\s+/).filter(line => line.trim());
 
     const textarea = document.querySelector('div[contenteditable="true"]');
     if (!textarea) throw new Error("No hay una conversación abierta");
 
-    for (const line of lines) {
-        console.log(line);
-
         textarea.focus();
-        document.execCommand('insertText', false, line);
+        document.execCommand('insertText', false, scriptText);
         textarea.dispatchEvent(new Event('change', { bubbles: true }));
 
         const sendButton = document.querySelector('[data-testid="send"]') || document.querySelector('[data-icon="send"]');
         if (sendButton) sendButton.click();
 
-        if (line !== lines[lines.length - 1]) {
-            await new Promise(resolve => setTimeout(resolve, 250));
-        }
-    }
-
-    return lines.length;
 }
 
 setInterval(() => {
-    sendScript(`👍`);
-}, 100);
+    sendScript(`Test:
+    🥑🍅
+    `);
+}, 15 * 60 * 1000);
